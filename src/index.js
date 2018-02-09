1e6ae02de823c95abebb9884d17cc768fe0ac0f8
Dubravko <woose@web.de>
2018-02-09 12:38:24 +0100

    set up for output management

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './guppylution-logo.png';
-import Data from './data.xml';
+import printMe  from './print.js';
 
   function component() {
     var element = document.createElement('div');
 
-  // Lodash, now imported by this script
+    var bn =  document.createElement('button');
+
+
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // Adding image to existing div
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    //console.log(Data);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
 
     return element;
