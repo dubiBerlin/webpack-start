f43a9a2dcac62b82ffbf6c88130cb61b32c58ecc
Dubravko <woose@web.de>
2018-02-09 12:03:34 +0100

    loading images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './guppylution-logo.png';
 
   function component() {
     var element = document.createElement('div');
@@ -7,6 +8,12 @@ import './style.css';
   // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // Adding image to existing div
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
 
     return element;
   }
